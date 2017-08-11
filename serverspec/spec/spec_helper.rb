7d9351b2bea93e85e083dbb1fb19630b3a1500cc
kongou-ae <y.matsumoto.ae@gmail.com>
2017-08-11 13:55:55 +0000

    fixed #1

--- serverspec/spec/spec_helper.rb
+++ serverspec/spec/spec_helper.rb
@@ -1,6 +1,23 @@
 require 'serverspec'
 require 'net/ssh'
 require 'winrm'
+require 'ansible/vault'
+
+# whether hosts is encrypt or not.
+if File.open('../ansible/hosts.yml').read.include?('$ANSIBLE_VAULT;') == true then
+  print 'spec_helper: innput the password of ansible-vault: '
+  system "stty -echo"
+  password = $stdin.gets.chop
+  system "stty echo"
+  puts ''
+  contents = Ansible::Vault.read(path: '../ansible/hosts.yml', password: password)
+else
+  contents = File.open('../ansible/hosts.yml').read
+end
+
+# inject property from hosts.yml
+properties = YAML.load(contents)
+set_property properties[ENV['ansible_role']]['vars']
 
 # Windows
 if ENV['ansible_connection'] == 'winrm' then
@@ -17,10 +34,10 @@ if ENV['ansible_connection'] == 'winrm' then
     user: ENV['ansible_user'],
     password: ENV['ansible_password'],
     endpoint: endpoint,
-    operation_timeout: 300,
+    operation_timeout: 60,
     no_ssl_peer_verification: true
   }
-  puts("Test of #{ENV['TARGET_HOST']}")
+  puts("Test for #{ENV['TARGET_HOST']}")
   winrm = WinRM::Connection.new(opts)
   Specinfra.configuration.winrm = winrm
   
@@ -31,7 +48,7 @@ else
   options = Net::SSH::Config.for(host)
   options[:user] = ENV['ansible_user']
   options[:password] = ENV['ansible_password']
-  puts("Test of #{ENV['TARGET_HOST']}")
+  puts("Test for #{ENV['TARGET_HOST']}")
   set :host,        options[:host_name] || host
   set :ssh_options, options      
 end
