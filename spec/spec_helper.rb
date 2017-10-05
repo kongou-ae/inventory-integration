7923d6169bdcdd064dd1860794d1a1932ceda28e
kongou-ae <y.matsumoto.ae@gmail.com>
2017-10-05 12:13:58 +0000

    fixed #5

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,12 +5,21 @@ require 'ansible/vault'
 
 vars = {}
 
+def read_with_vault(filename)
+  if File.open(filename).read.include?('$ANSIBLE_VAULT;') == true then
+    contents = Ansible::Vault.read(path: filename, password: ENV['VAULT_PASS'])
+  else
+    contents = File.open(filename).read
+  end
+  return contents
+end
+
 # inject the variables which set in roles directry
 Dir::foreach('roles') {|dir|
   if dir == ENV['ansible_role'] then
     Dir::foreach("roles/#{dir}/vars") {|f|
       if f =~ /.yml/ then
-        vars = YAML.load(File.open("roles/#{dir}/vars/#{f}").read)
+        vars = YAML.load(read_with_vault("roles/#{dir}/vars/#{f}"))
       end  
     }
   end    
@@ -19,11 +28,11 @@ Dir::foreach('roles') {|dir|
 # inject the variables which set in group vars
 Dir::foreach('group_vars') {|file|
   if file == "all.yml" then
-    vars = vars.merge(YAML.load(File.open("group_vars/all.yml").read))
+    vars = vars.merge(YAML.load(read_with_vault("group_vars/all.yml")))
   end
   
   if file == "#{ENV['ansible_role']}.yml" then
-    vars = vars.merge(YAML.load(File.open("group_vars/#{ENV['ansible_role']}.yml").read))
+    vars = vars.merge(YAML.load(read_with_vault("group_vars/#{ENV['ansible_role']}.yml")))
   end 
 }
 
@@ -57,8 +66,11 @@ else
   host = ENV['TARGET_HOST']
   options = Net::SSH::Config.for(host)
   options[:user] = ENV['ansible_user']
-  options[:password] = ENV['ansible_password']
+  options[:password] = ENV['ansible_password'] if not ENV['ansible_password'].nil?
+  options[:keys] = ENV['ansible_ssh_private_key_file'] if not ENV['ansible_ssh_private_key_file'].nil?
   puts("Test for #{ENV['TARGET_HOST']}")
   set :host,        options[:host_name] || host
-  set :ssh_options, options      
+  set :ssh_options, options
+  set :sudo_password, ENV['ansible_become_pass'] if not ENV['ansible_become_pass'].nil?
+  
 end
